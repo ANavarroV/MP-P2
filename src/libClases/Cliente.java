a7e79f0b5ca51be3f4425d63dea7574ae7d59c4a
Antonio Navarro <navarroventura.antonio@gmail.com>
2025-11-26 18:04:00 +0100

    Avanzando

--- src/libClases/Cliente.java
+++ src/libClases/Cliente.java
@@ -2,4 +2,22 @@ package libClases;
 
 public class Cliente {
 
+	private final String nif; //dni del cliente (letra incluida) (NO puede cambiar)
+	private final int codCliente; //codigo único (y fijo) generado por la aplicación
+	private String nombre; //nombre completo del cliente (SI se puede modificar)
+	private final Fecha fechaNac; //fecha nacimiento del cliente (NO se puede cambiar)
+	private final Fecha fechaAlta; //fecha de alta del cliente (SI se puede modificar)
+	
+	public Cliente (String nif, String nom, Fecha fNac, Fecha fAlta) {
+		
+		this.nif = nif;
+		nombre = nom;
+		fechaNac = fNac;
+		fechaAlta = fAlta;
+	
+	} //constructor
+	
+	public Cliente (String NIF, String nom, Fecha fNac); //constructor
+	public String toString(); //devuelve una cadena con la información del cliente
+	
 }
---
+++ src/libPruebas/Prueba1.java
@@ -0,0 +1,12 @@
+package libPruebas;
+
+import libClases.*;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola");
+	}
+
+}
